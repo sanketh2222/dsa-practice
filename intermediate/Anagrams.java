3c1f5c682bacaa859878c68be1dad8520b30accb
Sanketh S <sanketh.s@rubixds.com>
2023-01-01 14:02:59 +0530

    small optimization

--- intermediate/Anagrams.java
+++ intermediate/Anagrams.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Anagrams {
 
-    //Brute force
+    // Brute force
     public int solve(String A, String B) {
         Map<Character, Integer> fMap = new HashMap<>();
         for (char ch : A.toCharArray()) {
@@ -21,29 +21,28 @@ public class Anagrams {
             if (fMap.containsKey(ch)) {
                 int freq = fMap.get(ch);
                 fMap.put(ch, ++freq);
-            } else {
-                fMap.put(ch, 1);
+            } else
+                return 0;
 
-            }
         }
 
         return fMap.values().stream().anyMatch(x -> (x & 1) == 1) ? 0 : 1;
 
     }
 
-    //Optimized approach
-    public int checkAnagram(String A,String B) {
+    // Optimized approach
+    public int checkAnagram(String A, String B) {
         int[] arr = new int[26];
 
         for (char ch : A.toCharArray()) {
-            int charIndex = ch -'0' - 49;
+            int charIndex = ch - '0' - 49;
             int freq = arr[charIndex];
             arr[charIndex] = ++freq;
 
         }
 
         for (char ch : B.toCharArray()) {
-            int charIndex = ch -'0' - 49;
+            int charIndex = ch - '0' - 49;
             int freq = arr[charIndex];
             if (freq == 0)
                 return 0;
@@ -60,17 +59,17 @@ public class Anagrams {
     }
 
     public static void main(String[] args) {
-        String A = "cat";
-        String B = "bat";
+        String A = "secure";
+        String B = "rescue";
         Anagrams anagrams = new Anagrams();
-        int ans = anagrams.checkAnagram(A, B);
+        int ans = anagrams.solve(A, B);
         System.out.println(ans);
 
         int[] arr = new int[26];
         char ch = 'a';
         char z = 'z';
-        System.out.println(ch -'0' - 49);
-        System.out.println(z -'0' - 49);
+        System.out.println(ch - '0' - 49);
+        System.out.println(z - '0' - 49);
 
     }
 }
